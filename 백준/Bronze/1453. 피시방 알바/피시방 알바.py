fdbafd1a78938d27f65f64d03afcab836ce604ae
Lightness-of-Being <121467589+Lightness-of-Being@users.noreply.github.com>
2023-01-26 14:03:07 +0900

    [Bronze II] Title: 피시방 알바, Time: 40 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.py
@@ -0,0 +1,10 @@
+table = [0]*100
+res = 0
+T = int(input())
+list_ = list(map(int, input().split()))
+for i in list_:
+    if table[i - 1] == 0:
+        table[i - 1] = 1
+    else:
+        res += 1
+print(res)
