c081d6634b859ba0a48b79438d12f35233f636b0
Lightness-of-Being <121467589+Lightness-of-Being@users.noreply.github.com>
2023-01-26 13:54:37 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -0,0 +1,12 @@
+angle = []
+for i in range(3):
+    angle.append(int(input()))
+if sum(angle) != 180:
+    print('Error')
+else:
+    if angle[0] == angle[1] == angle[2]:
+        print('Equilateral')
+    elif angle[0] != angle[1] != angle[2] != angle[0]:
+        print('Scalene')
+    else:
+        print('Isosceles')
